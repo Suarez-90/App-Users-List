fde97c0153db3c4df669567324d678d0f7d73a0e
Suarez-90 <ssuarezcalanas@gmail.com>
2025-06-06 19:40:50 -0400

    initial app list of users

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>     
+      <header>
+        <h1>Listado de Usuarios</h1>
+      </header>
+     
+    </>
+  )
+}
+
+export default App
